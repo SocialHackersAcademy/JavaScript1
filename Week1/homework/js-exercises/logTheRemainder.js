485487a14b76b14b14693124cd05af3116098e80
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-02-07 11:13:38 -0600

    Add files via upload

---
+++ Week1/homework/js-exercises/logTheRemainder.js
@@ -0,0 +1,11 @@
+
+const myFirstArray = [1, 2, 3, 4];
+const mySecondArray = [1, 2, 3, 4, 5, 6, 7];
+
+console.log("The length of myFirstArray is " + myFirstArray.length);
+console.log("The length of mySecondArray is " + mySecondArray.length);
+
+if(myFirstArray.length == mySecondArray.length)
+    console.log("They are the same!");
+else
+    console.log("Two different sizes");
---
+++ Week1/homework/js-exercises/typeChecker.js
@@ -0,0 +1,15 @@
+
+/* If x equals 7, and the only other statement is x = x % 3, what would be the value of x after the 
+calculation? */
+console.log("2 times 3 is equal to 6, and the remainder to reach 7 is 1, so x = 1:");
+console.log(7 % 3);
+
+/* If y equals 21, and the only other statement is y = y % 4, what would be the value of y after the 
+calculation? */
+console.log("5 times 4 is equal to 20, and the remainder to reach 21 is 1, so y = 1:");
+console.log(21 % 4);
+
+/* If z equals 13, and the only other statement is z = z % 2, what would be the value of z after the 
+calculation? */
+console.log("6 times 2 is equal to 12, and the remainder to reach 13 is 1, so z = 1:");
+console.log(13 % 2);
